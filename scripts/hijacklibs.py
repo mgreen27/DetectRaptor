374259f2ab973c0988c36dad438915c5128912c4
Matthew Green <mgreen27@gmail.com>
2023-01-05 00:31:02 +1100

    Add HijacklibsMFT

---
+++ scripts/hijacklibs.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python3
+"""
+This script builds Hijacklibs based Velociraptor artifacts
+
+https://hijacklibs.net/
+
+Current csv structure:
+    row[0]  = Name
+    row[1]  = Author
+    row[2]  = Created
+    row[3]  = Vendor
+    row[4]  = CVE
+    row[5]  = ExpectedLocations
+    row[6]  = VulnerableExecutablePath
+    row[7]  = VulnerableExecutableType
+    row[8]  = VulnerableExecutableAutoElevated
+    row[9]  = VulnerableExecutablePrivilegeEscalation
+    row[10] = VulnerableExecutableCondition
+    row[11] = VulnerableExecutableSHA256
+    row[11] = VulnerableExecutableEnvironmentVariable
+    row[12] = Resources
+    row[13] = Acknowledgements
+    row[14] = URL
+"""
+
+import requests
+import csv
+import sys
+import re
+import os
+import yaml
+import pandas as pd
+
+
+# set variables
+hijacklibs_url = 'https://hijacklibs.net/api/hijacklibs.csv'
+ioc_csv = '../csv/hijacklibs.csv'
+ioc_csv2 = '../csv/hijacklibs_environment.csv'
+
+template_vql = '../templates/Hijacklibs.tempalte'
+output_path = '../vql/'
+
+
+def build_vql(lookup_table,template,output_path):
+
+    vql = (template % dict(
+        ioc=''.join(["        " + x for x in lookup_table])
+      ))
+
+    name = yaml.load(vql, Loader=yaml.BaseLoader)['name']
+    output_path = output_path +  name.split('.')[-1] + '.yaml'
+    
+    print('\tWriting to:' + output_path)
+
+    with open(output_path, 'w') as outfile:
+      outfile.write(vql)
+
+
+def convert_env(variable):
+    variable = re.sub(r'\\', r'\\\\', variable)
+    variable = re.sub(r'\.', '\\.', variable)
+    variable = re.sub(r'\(', '\\(', variable)
+    variable = re.sub(r'\)', '\\)', variable)
+    variable = re.sub('%WINDIR%', r'\\\\Windows', variable)
+    variable = re.sub('%SYSTEM32%', r'\\\\Windows\\\\System32', variable)
+    variable = re.sub('%SYSWOW64%', r'\\\\Windows\\\\SysWOW64', variable)
+    variable = re.sub('%VERSION%', r'[^\\\\]+', variable)
+    variable = re.sub('%PROGRAMFILES%', r'\\\\Windows\\\\Program Files( \\(x86\\))?', variable)
+    variable = re.sub('%PROGRAMDATA%', r'\\\\Programdata', variable)
+    variable = re.sub('%APPDATA%', r'\\\\(Users\\\\[^\\\\]+|windows\\\\(System32|SysWOW64)\\\\config\\\\systemprofile)\\\\AppData\\\\[^\\\\]+', variable)
+    variable = re.sub('%LOCALAPPDATA%', r'\\\\(Users\\\\[^\\\\]+|windows\\\\(System32|SysWOW64)\\\\config\\\\systemprofile)\\\\AppData\\\\[^\\\\]+', variable)
+
+    return variable
+
+
+# function that appends to our final list data 
+def append_csv_data(x, grouped_name):
+    usable_data.append(
+        [
+            grouped_name,
+            '|'.join(x['Vendor'].unique().tolist()),
+            '|'.join(x['ExpectedLocation'].unique().tolist()),
+            '|'.join(x['ExecutablePath'].unique().tolist()),
+            '|'.join(x['Type'].unique().tolist()),
+            '|'.join(x['ExecutableSHA256'].unique().tolist()),
+            '|'.join(x['Url'].unique().tolist())
+        ])
+
+
+if __name__ == "__main__":
+    print('Building HijackLibs artifact')
+
+    # first download hijacklibs iocs and build regex csv
+    csv_data = requests.get(hijacklibs_url).content.decode("utf-8")
+    
+    flattened_data = []
+    usable_data = []
+
+    #for row in csv_data.split('\n'):
+    for row in csv.reader(csv_data.split('\n')):
+        if not row:
+            continue
+        if row[0] == 'Name':
+            # firstly check headers are expected and set headers
+            if not row == ['Name', 'Author', 'Created', 'Vendor', 'CVE', 'ExpectedLocations', 'VulnerableExecutablePath', 'VulnerableExecutableType', 'VulnerableExecutableAutoElevated', 'VulnerableExecutablePrivilegeEscalation', 'VulnerableExecutableCondition', 'VulnerableExecutableSHA256', 'VulnerableExecutableEnvironmentVariable', 'Resources', 'Acknowledgements', 'URL']:
+                print('API may have changed')
+                print(row)
+            #[row[0],row[3],row[5],row[6],row[7],row[11],row[15]]
+            flattened_data.append(['DllName','Vendor','ExpectedLocation','ExecutablePath','Type','ExecutableSHA256','Url'])
+            usable_data.append(['DllName','Vendor','ExpectedLocation','ExecutablePath','Type','ExecutableSHA256','Url'])
+        elif row[7] == 'Environment Variable':
+            continue
+        else:
+            # normalise paths to regex
+            row[5] = convert_env(row[5])
+            row[6] = convert_env(row[6])
+
+            # flatten ExpectedLocations and VulnerableExecutablePath
+            if ',' in row[5] and ',' in row[6]:
+                print('value5 and value6')
+                for value5 in row[5].split(','):
+                    for value6 in row[6].split(','):
+                        flattened_data.append([row[0],row[3],value5.lstrip(),value6.lstrip(),row[7],row[11],row[15]])
+            # flatten ExpectedLocations
+            elif ',' in row[5]:
+                for value5 in row[5].split(','):
+                    flattened_data.append([row[0],row[3],value5.lstrip(),row[6],row[7],row[11],row[15]])
+
+            # flatten VulnerableExecutablePath
+            elif ',' in row[6]:
+                print('value6')
+                for value6 in row[6].split(','):
+                    flattened_data.append([row[0],row[3],row[5],value6.lstrip(),row[7],row[11],row[15]])
+            else:
+                flattened_data.append([row[0],row[3],row[5],row[6],row[7],row[11],row[15]])
+
+    
+    #use pandas to group by DllName and join hijacklibs data
+    df = pd.DataFrame(flattened_data[1:], columns=flattened_data[0])
+    grouped = df.groupby('DllName')
+    grouped.apply(lambda x: append_csv_data(x, x.name))
+
+    
+    # finally we write to csv and build VQL
+    with open(ioc_csv, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(usable_data)
+    
+    # grab csv contents and split to list of lines
+    with open(ioc_csv, 'r') as file:
+      lookup_table = file.readlines()
+
+    #grab VQL template
+    with open(template_vql, 'r') as file:
+      template = file.read()
+
+    # build vql artifacts
+    build_vql(lookup_table,template,output_path)
