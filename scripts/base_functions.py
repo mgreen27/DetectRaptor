ee581e446b0e07f495631082d819b1931894e199
Matt Green <mgreen27@gmail.com>
2025-03-06 11:48:33 +1100

    add lolrmm project

--- scripts/base_functions.py
+++ scripts/base_functions.py
@@ -55,3 +55,12 @@ def shasum(filename):
         while n := f.readinto(mv):
             h.update(mv[:n])
     return h.hexdigest()
+
+def sha256sum(filename):
+    h = hashlib.sha256()
+    b = bytearray(128 * 1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        while n := f.readinto(mv):
+            h.update(mv[:n])
+    return h.hexdigest()
